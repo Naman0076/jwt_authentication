02c34cee911c2895b0f783af453c82cd4a30a0bb
Naman Kaushik <naman.kaushik@impactqa.com>
2025-03-27 10:41:08 +0530

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
 from flask import Flask, request
-from models import db, User
 from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 import datetime
+import bcrypt
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "e251d7fc2b7d8924564bdea020f90b99"
+app = Flask(__name__) 
+app.config["JWT_SECRET_KEY"] = "044a7dcd3dff50c8130da541243d7db7"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = datetime.timedelta(minutes=5)
 app.config["JWT_REFRESH_TOKEN_EXPIRES"] = datetime.timedelta(hours=1)
+
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        """Hashes the password using bcrypt and stores it in the password_hash field."""
+        salt = bcrypt.gensalt()
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+
+    def check_password(self, password):
+        """Checks the password against the stored bcrypt hash."""
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
+
+    def toJSON(self):
+        """Returns a JSON-serializable representation of the User."""
+        return {
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email
+        }
+    
 
 jwt = JWTManager(app)
 
@@ -62,7 +102,7 @@ def signin():
     if not data.get("email", None) or not data.get("password", None):
         return {"error": "Invalid data"}, 400
     
-    user = db.query(User).filter_by(email=data['email']).first()
+    user = User.query.filter_by(email=data['email']).first()
 
     if user:
         if user.check_password(data["password"]):
